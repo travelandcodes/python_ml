7700a475a69cdd43a709943cafcf6504eb0ae810
Nikhil K Singh <37348724+Nikhil-K-Singh@users.noreply.github.com>
2020-05-14 01:53:15 +0530

    removing the incorrect imputation made by Itua....
    you can substitute the missing values by the mean of column as mentioned here...
    
    now this is the final 10/10 answer.

--- temp.py
+++ temp.py
@@ -1,30 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-dataset = pd.read_csv('Data.csv');
-
-X = dataset.iloc[:,:-1].values
-Y = dataset.iloc[:,3].values
+dataset = pd.read_csv('Data.csv')
 
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
-imputer = SimpleImputer(missing_values = np.nan , strategy = "mean")
-imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-from sklearn.preprocessing import LabelEncoder , OneHotEncoder
+# filling the nan values with themean of the respective column
+dataset['Age']=dataset['Age'].fillna(dataset['Age'].mean())
+dataset['Salary']=dataset['Salary'].fillna(dataset['Salary'].mean())
 
-labelencoder_X = LabelEncoder()
-X[: , 0] = labelencoder_X.fit_transform(X[: , 0])
-onehotencoder = OneHotEncoder(categories = X[0])
-X = onehotencoder.fit_transform(X).toarray()
-labelencoder_Y = LabelEncoder()
+# instantiating the OneHotEncoder
+onehotencoder = OneHotEncoder()
 
-Y[: , 0] = labelencoder_Y.fit_transform(Y[: , 0])
+#fitting the data
+spare=onehotencoder.fit_transform(dataset.Country.values.reshape(-1,1)).toarray()
+dataset.Country.values.reshape(-1,1)
+
+#generating column names
+columns = ["Country_"+str(int(i)) for i in range(dataset.shape[1]-1)]
+
+#creating a new dataframe for the one hot encoded data
+dfOneHot= pd.DataFrame(spare,columns=columns)
+
+#concatenating the two dataframes
+df = pd.concat([dataset, dfOneHot], axis=1)
+
+# removing the country column from the final viz
+df= df.drop(['Country'], axis=1)
+
+# calling the final output
+df
